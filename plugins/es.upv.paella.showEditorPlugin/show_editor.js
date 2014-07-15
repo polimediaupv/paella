3ade1f68fa332ebfe8d7df1fe21d5b7fed33d238
Miguel Escrivá <miesgre@gmail.com>
2014-07-15 10:59:45 +0200

    Replace paella.utils.userAgent by base.userAgent

--- plugins/es.upv.paella.showEditorPlugin/show_editor.js
+++ plugins/es.upv.paella.showEditorPlugin/show_editor.js
@@ -14,7 +14,7 @@ paella.ShowEditorPlugin = Class.create(paella.VideoOverlayButtonPlugin,{
 	getDefaultToolTip:function() { return base.dictionary.translate("Enter editor mode"); },
 
 	checkEnabled:function(onSuccess) {
-		onSuccess(paella.editor && paella.player.config.editor && paella.player.config.editor.enabled && !paella.utils.userAgent.browser.IsMobileVersion &&
+		onSuccess(paella.editor && paella.player.config.editor && paella.player.config.editor.enabled && !base.userAgent.browser.IsMobileVersion &&
 			(paella.initDelegate.initParams.accessControl.permissions.canWrite || this.config.alwaysVisible) && !paella.player.isLiveStream());
 	},
 
