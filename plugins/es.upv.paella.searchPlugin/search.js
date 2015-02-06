f9660a68627592c1b430a7264accb96b5c8ed856
Sergio Sanchez Ortiz <sersanor@gmail.com>
2015-02-06 15:02:09 +0100

    Sort + Color + UI Design

--- plugins/es.upv.paella.searchPlugin/search.js
+++ plugins/es.upv.paella.searchPlugin/search.js
@@ -1,29 +1,75 @@
 Class ("paella.plugins.SearchPlugin", paella.ButtonPlugin,{
-	_defaultState: false,
+	_open: false,
+	_sortDefault: 'score',
+	_colorSearch: true,
 
-	getAlignment:function() { return 'left'; },
+
+	getAlignment:function() { return 'right'; },
 	getSubclass:function() { return 'searchButton'; },
 	getName:function() { return "es.upv.paella.searchPlugin"; },
 	getButtonType:function() { return paella.ButtonPlugin.type.popUpButton; },	
-	getDefaultToolTip:function() { return base.dictionary.translate("Search text on captions"); },
-	getIndex:function() {return 200;},
+	getDefaultToolTip:function() { return base.dictionary.translate("Search"); },
+	getIndex:function() {return 510;},
 
 	checkEnabled:function(onSuccess) {
 		onSuccess(true);
 	},
 
 	setup:function() {
+		var self = this;
 		$('.searchButton').click(function(event){
-			if(paella.player.videoContainer.paused()){
+			if(self._open){
 				paella.events.trigger(paella.events.play);
 				paella.keyManager.enabled = true;
+				self._open = false;
 			}
 			else {
-					paella.events.trigger(paella.events.pause);
-					paella.keyManager.enabled = false;
-
+				paella.events.trigger(paella.events.pause);
+				paella.keyManager.enabled = false;
+				self._open = true;
+				setTimeout(function(){
+   					 $("#searchBarInput").focus();
+				}, 0);
 				}
 		});
+	},
+
+
+	search:function(text,cb){
+		setTimeout(function(){
+			cb(true,[
+				{
+				time:124,
+				caption:"Hola mundo que tal?",
+				score:0.2
+				},
+				{
+				time:300,
+				caption:"Hola mundo que tal?",
+				score:0.5
+				},
+				{
+				time:1300,
+				caption:"Hola mundo que tal?",
+				score:0.75
+				},
+				{
+				time:487,
+				caption:"Hola mundo que tal?",
+				score:0.15
+				},
+				{
+				time:679,
+				caption:"Hola mundo que tal?",
+				score:0.6
+				},
+				{
+				time:1174,
+				caption:"Hola mundo que tal?",
+				score:0.3
+				}
+				]);
+		}, 2000);
 	},
 
 	buildContent:function(domElement) {
@@ -32,50 +78,102 @@ Class ("paella.plugins.SearchPlugin", paella.ButtonPlugin,{
 		//SEARCH CONTAINER
 		var searchPluginContainer = document.createElement('div');
         searchPluginContainer.className = 'searchPluginContainer';
-
-	        //SEARCH BAR
-	        var searchBar = document.createElement('div');
-	        searchBar.className = 'searchBar';
-	        searchPluginContainer.appendChild(searchBar);
-
-		        //INPUT
-		        var input = document.createElement("input");
-		        input.className = "searchBarInput";
-		        input.type = "text";
-		        input.name = "searchString";
-		        input.placeholder = "ex: Fourier, Matrix, Hoffman... etc";
-		        searchBar.appendChild(input);
 	        
 	        //SEARCH BODY
 	        var searchBody = document.createElement('div');
 	        searchBody.className = 'searchBody';
 	        searchPluginContainer.appendChild(searchBody);
 
-	        //MAKE THE SEARCH AND GET THE RESULTS AND FILL;
-	        var elements = 5;
 
-	        for(var i=0; i<elements; i++){
-	        	var sBodyInnerContainer = document.createElement('div');
-	        	sBodyInnerContainer.className = 'sBodyInnerContainer';
+	    //SEARCH BAR
+	    var searchBar = document.createElement('div');
+        searchBar.className = 'searchBar';
+        searchPluginContainer.appendChild(searchBar);
 
-	        	var sBodyPicture = document.createElement('img');
-	        	sBodyPicture.className = 'sBodyPicture';
-	        	sBodyPicture.src ='http://paellaplayer.upv.es/demo/repository/belmar-multiresolution/slides/46561b90-85b3-4ad7-a986-cdd9b52ae02b/presentation_cut.jpg';
+	        //INPUT
+	        var input = document.createElement("input");
+	        input.className = "searchBarInput";
+	        input.type = "text";
+	        input.id ="searchBarInput";
+	        input.name = "searchString";
+	        input.placeholder = base.dictionary.translate("Search");
+	        searchBar.appendChild(input);
 
-	        	var sBodyText = document.createElement('p');
-	        	sBodyText.className = 'sBodyText';
-	        	sBodyText.innerHTML = " Something of text";
+	        $(input).change(function(){
+	        	var text = $(input).val();
 
-	        	var sBodyTime = document.createElement('p');
-	        	sBodyTime.className = 'sBodyTime';
-	        	sBodyTime.innerHTML = " 00:13:01";
+				thisClass.search(text,function(err, results){
+				//BUILD SEARCH RESULTS
+				if(err){
+					for(var i=0; i<results.length; i++){
 
-	        	sBodyInnerContainer.appendChild(sBodyPicture);
-	        	sBodyInnerContainer.appendChild(sBodyText);
-	        	sBodyInnerContainer.appendChild(sBodyTime);
+			        	//SEARCH SORT TYPE (TIME oR SCoRE)
+			        	if(thisClass._sortDefault == 'score') {
+				        	results.sort(function(a,b){
+								return b.score - a.score;
+							});
+				        }
+						if(thisClass._sortDefault == 'time') {
+				        	results.sort(function(a,b){
+								return a.time - b.time;
+							});
+				    	}
 
-	        	searchBody.appendChild(sBodyInnerContainer);
-	        }
+						var sBodyInnerContainer = document.createElement('div');
+			        	sBodyInnerContainer.className = 'sBodyInnerContainer';
+			        	//COLOR?
+			        	//g rgba(0, 255, 0, 0.2)
+						//r rgba(255, 0, 0, 0.2);
+			        	if(thisClass._colorSearch){ 
+
+			        		if(results[i].score <= 0.3)
+			        		sBodyInnerContainer.style.backgroundColor="rgba(255, 0, 0, 0.2)";
+
+			        		if(results[i].score >= 0.7)
+			        		sBodyInnerContainer.style.backgroundColor="rgba(0, 255, 0, 0.2)";
+			        	}
+
+			        	var TimePicContainer = document.createElement('div');
+			        	TimePicContainer.className = 'TimePicContainer';
+
+			        	var sBodyPicture = document.createElement('img');
+			        	sBodyPicture.className = 'sBodyPicture';
+			        	sBodyPicture.src ='http://paellaplayer.upv.es/demo/repository/belmar-multiresolution/slides/46561b90-85b3-4ad7-a986-cdd9b52ae02b/presentation_cut.jpg';
+
+			        	var sBodyText = document.createElement('p');
+			        	sBodyText.className = 'sBodyText';
+			        	sBodyText.innerHTML = "<span class=\"timeSpan\">"+results[i].time+"</span>"+results[i].caption;
+
+
+			        	TimePicContainer.appendChild(sBodyPicture);
+			        	
+
+			        	sBodyInnerContainer.appendChild(TimePicContainer);
+			        	sBodyInnerContainer.appendChild(sBodyText);
+			        	
+
+			        	searchBody.appendChild(sBodyInnerContainer);
+			        	//ADD SECS TO DOM FOR EASY HANDLE
+			        	sBodyInnerContainer.setAttribute('sec',results[i].time);
+
+			        	//jQuery Binds for the search
+			        	$(sBodyInnerContainer).hover(
+			        		function(){ 
+			        			$(this).addClass('hover');	           		
+			        		},
+			        		function(){ $(this).removeClass('hover'); 
+			        		}
+			        	);
+
+			        	$(sBodyInnerContainer).click(function(){ 
+			        		var sec = $(this).attr("sec");
+			        		paella.player.videoContainer.seekToTime(sec);
+			        	});
+					}
+						
+					}
+			    });
+			});
 
         domElement.appendChild(searchPluginContainer);
 
