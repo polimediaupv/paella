7a5f335ee068b06f68495edd5f472f66c495b8e7
Miguel Escrivá <miesgre@gmail.com>
2015-06-05 11:37:17 +0200

    checking breaks plugins

--- plugins/es.upv.paella.breakPlugins/editor.js
+++ plugins/es.upv.paella.breakPlugins/editor.js
@@ -65,7 +65,7 @@ Class ("paella.plugins.BreaksEditorPlugin",paella.editor.MainTrackPlugin, {
 	},
 
 	getName:function() {
-		return "es.upv.paella.editor.trackBreaks";
+		return "es.upv.paella.editor.trackBreaksPlugin";
 	},
 
 	getTrackName:function() {
@@ -131,75 +131,3 @@ Class ("paella.plugins.BreaksEditorPlugin",paella.editor.MainTrackPlugin, {
 });
 
 paella.plugins.breaksEditorPlugin = new paella.plugins.BreaksEditorPlugin();
-
-
-Class ("paella.plugins.BreaksPlayerPlugin",paella.EventDrivenPlugin,{
-	breaks:null,
-	lastEvent:0,
-	visibleBreaks:null,
-
-	getName:function() { return "es.upv.paella.BreaksPlayerPlugin"; },
-	checkEnabled:function(onSuccess) {
-		var This = this;
-		this.breaks = [];
-		this.visibleBreaks = [];
-		paella.data.read('breaks',{id:paella.initDelegate.getId()},function(data,status) {
-			if (data && typeof(data)=='object' && data.breaks && data.breaks.length>0) {
-				This.breaks = data.breaks;
-			}
-			onSuccess(true);
-		});
-	},
-
-	getEvents:function() { return [paella.events.timeUpdate]; },
-
-	onEvent:function(eventType,params) {
-		this.checkBreaks(params);
-	},
-
-	checkBreaks:function(params) {
-		var a;
-		for (var i=0; i<this.breaks.length; ++i) {
-			a = this.breaks[i];
-
-			if (a.s<params.currentTime && a.e>params.currentTime) {
-				this.showBreaks(a);
-			} else if (a.s.toFixed(0) == params.currentTime.toFixed(0)){
-				this.avoidBreak(a);
-			}
-		}
-
-		for (var key in this.visibleBreaks) {
-			if (typeof(a)=='object') {
-				a = this.visibleBreaks[key];
-				if (a && (a.s>=params.currentTime || a.e<=params.currentTime)) {
-					this.removeBreak(a);
-				}
-			}
-		}
-	},
-
-	showBreaks:function(br) {
-		if (!this.visibleBreaks[br.s]) {
-			var rect = {left:100,top:350,width:1080,height:20};
-			br.elem = paella.player.videoContainer.overlayContainer.addText(br.content,rect);
-			br.elem.className = 'textBreak';
-			this.visibleBreaks[br.s] = br;
-		}
-	},
-
-	removeBreak:function(br) {
-		if (this.visibleBreaks[br.s]) {
-			var elem = this.visibleBreaks[br.s].elem;
-			paella.player.videoContainer.overlayContainer.removeElement(elem);
-			this.visibleBreaks[br.s] = null;
-		}
-	},
-
-	avoidBreak:function(br){
-		var newTime = br.e;
-		paella.events.trigger(paella.events.seekToTime,{time:newTime});
-	}
-});
-
-paella.plugins.breaksPlayerPlugin = new paella.plugins.BreaksPlayerPlugin();
