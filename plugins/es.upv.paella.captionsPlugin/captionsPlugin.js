602548c9e17ac282592c29a6d7a2ecf59a5afa24
Sergio Sánchez Ortiz <sersanor@gmail.com>
2015-06-18 10:21:15 +0200

    searchBar bug fix

--- plugins/es.upv.paella.captionsPlugin/captionsPlugin.js
+++ plugins/es.upv.paella.captionsPlugin/captionsPlugin.js
@@ -213,7 +213,9 @@ Class ("paella.plugins.CaptionsPlugin", paella.ButtonPlugin,{
 		//captions CONTAINER
 		thisClass._parent = document.createElement('div');
         thisClass._parent.className = 'captionsPluginContainer';  
-	    
+	    //captions BAR
+	   	thisClass._bar = document.createElement('div');
+        thisClass._bar.className = 'captionsBar';
         //captions BODY
         if(thisClass._searchOnCaptions){
 	        thisClass._body = document.createElement('div');
@@ -223,11 +225,6 @@ Class ("paella.plugins.CaptionsPlugin", paella.ButtonPlugin,{
 	        $(thisClass._body).scroll(function(){
 	        	thisClass._autoScroll = false;
 	        });
-
-	    //captions BAR
-	    thisClass._bar = document.createElement('div');
-        thisClass._bar.className = 'captionsBar';
-        thisClass._parent.appendChild( thisClass._bar);	
 
 	        //INPUT
 	        thisClass._input = document.createElement("input");
@@ -274,6 +271,7 @@ Class ("paella.plugins.CaptionsPlugin", paella.ButtonPlugin,{
         });
 
          thisClass._bar.appendChild(thisClass._select);
+         thisClass._parent.appendChild( thisClass._bar);
 
         //jQuery SELECT
         $(thisClass._select).change(function(){
