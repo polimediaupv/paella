c75c99b4c03e0de03b66095bf6f2870acc639d4f
Miguel Escrivá <miesgre@gmail.com>
2015-06-26 09:13:26 +0200

    Fixed typo

--- plugins/es.upv.paella.search.CaptionsSearchPlugIn/captionsSearch.js
+++ plugins/es.upv.paella.search.CaptionsSearchPlugIn/captionsSearch.js
@@ -2,7 +2,7 @@
 // Caption Search
 /////////////////////////////////////////////////
 Class ("paella.plugins.CaptionsSearchPlugIn", paella.SearchServicePlugIn, {
-	getName: function() { return "es.upv.paella.search.CaptionsSearchPlugin"; },
+	getName: function() { return "es.upv.paella.search.captionsSearchPlugin"; },
 
 	search: function(text, next) {
 		paella.captions.search(text, next);
