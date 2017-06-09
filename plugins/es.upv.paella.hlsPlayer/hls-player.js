39b9bb558dd1ecdb780901828cf1a5ff96d52e53
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-06-09 09:09:14 +0200

    Quick test implemented on HLS plugin to fall back to HTML5 video on iOS devices.

--- plugins/es.upv.paella.hlsPlayer/hls-player.js
+++ plugins/es.upv.paella.hlsPlayer/hls-player.js
@@ -19,53 +19,58 @@ Class ("paella.HLSPlayer", paella.Html5Video,{
 	},
 	
 	load:function() {
-		let This = this;
-		return new Promise((resolve,reject) => {
-			var source = this._stream.sources.hls;
-			if (source && source.length>0) {
-				source = source[0];
-				this._loadDeps()
-					.then(function(Hls) {
-						if(Hls.isSupported()) {
-							This._hls = new Hls();
-							This._hls.loadSource(source.src);
-							This._hls.attachMedia(This.video);
-							
-							This._hls.on(Hls.Events.LEVEL_SWITCHED, function(ev, data) {
-								This.qualityIndex = data.level;
-								This.setQuality(data.level);
-							});
-							
-							This._hls.on(Hls.Events.ERROR, function (event, data) {
-								if (data.fatal) {
-									switch(data.type) {
-									case Hls.ErrorTypes.NETWORK_ERROR:
-										base.log.error("paella.HLSPlayer: Fatal network error encountered, try to recover");
-										This._hls.startLoad();
-										break;
-									case Hls.ErrorTypes.MEDIA_ERROR:
-										base.log.error("paella.HLSPlayer: Fatal media error encountered, try to recover");
-										This._hls.recoverMediaError();
-										break;
-									default:
-										base.log.error("paella.HLSPlayer: Fatal Error. Can not recover");
-										This._hls.destroy();
-										break;
-									}
-								}
-							});
-							This._hls.on(Hls.Events.MANIFEST_PARSED,function() {
-								This._deferredAction(function() {
-									resolve();
+		if (base.userAgent.system.iOS) {
+			return this.parent();
+		}
+		else {
+			let This = this;
+			return new Promise((resolve,reject) => {
+				var source = this._stream.sources.hls;
+				if (source && source.length>0) {
+					source = source[0];
+					this._loadDeps()
+						.then(function(Hls) {
+							if(Hls.isSupported()) {
+								This._hls = new Hls();
+								This._hls.loadSource(source.src);
+								This._hls.attachMedia(This.video);
+								
+								This._hls.on(Hls.Events.LEVEL_SWITCHED, function(ev, data) {
+									This.qualityIndex = data.level;
+									This.setQuality(data.level);
 								});
-							});
-						}
-					});
-			}
-			else {
-				reject(new Error("Invalid source"));
-			}
-		});
+								
+								This._hls.on(Hls.Events.ERROR, function (event, data) {
+									if (data.fatal) {
+										switch(data.type) {
+										case Hls.ErrorTypes.NETWORK_ERROR:
+											base.log.error("paella.HLSPlayer: Fatal network error encountered, try to recover");
+											This._hls.startLoad();
+											break;
+										case Hls.ErrorTypes.MEDIA_ERROR:
+											base.log.error("paella.HLSPlayer: Fatal media error encountered, try to recover");
+											This._hls.recoverMediaError();
+											break;
+										default:
+											base.log.error("paella.HLSPlayer: Fatal Error. Can not recover");
+											This._hls.destroy();
+											break;
+										}
+									}
+								});
+								This._hls.on(Hls.Events.MANIFEST_PARSED,function() {
+									This._deferredAction(function() {
+										resolve();
+									});
+								});
+							}
+						});
+				}
+				else {
+					reject(new Error("Invalid source"));
+				}
+			});
+		}
 	},
 	
 	getQualities:function() {
