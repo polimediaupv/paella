b7a06c7e533913d5c4c0f231833aaf5ebb456e2f
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-07-10 11:16:51 +0200

    traceur-runtime.min.js removed and generated from npm package

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const	gulp = require('gulp'),
 		traceur = require('gulp-traceur'),
 		merge = require('gulp-merge-json'),
 		fs = require('fs'),
+		minify = require('gulp-minify'),
 		uglify = require('gulp-uglify'),
 		flatten = require('gulp-flatten'),
 		path = require('path'),
@@ -128,6 +129,7 @@ gulp.task("copy", function() {
 			.pipe(gulp.dest(`${config.outDir}player/`)),
 
 		gulp.src('node_modules/traceur/bin/traceur-runtime.js')
+			.pipe(minify({ ext: { min: '.min.js' }}))
 			.pipe(gulp.dest(`${config.outDir}player/javascript`))
 	];
 
