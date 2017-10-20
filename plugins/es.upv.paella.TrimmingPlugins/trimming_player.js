15319c67ba910fc763ddd62d8e542cd72ae46532
Fernando Serrano Carpena <ferserc1@gmail.com>
2017-10-20 11:04:01 +0200

    Plugin refactoring: search, social, theme chooser, trimming loader

--- plugins/es.upv.paella.TrimmingPlugins/trimming_player.js
+++ plugins/es.upv.paella.TrimmingPlugins/trimming_player.js
@@ -1,46 +1,46 @@
-Class ("paella.plugins.TrimmingLoaderPlugin",paella.EventDrivenPlugin,{
-	
-	getName:function() { return "es.upv.paella.trimmingPlayerPlugin"; },
+paella.addPlugin(function() {
+	return class TrimmingLoaderPlugin extends paella.EventDrivenPlugin {
 		
-	getEvents:function() { return [paella.events.controlBarLoaded, paella.events.showEditor, paella.events.hideEditor]; },
-
-	onEvent:function(eventType,params) {
-		switch (eventType) {
-			case paella.events.controlBarLoaded:
-				this.loadTrimming();
-				break;
-			case paella.events.showEditor:
-				paella.player.videoContainer.disableTrimming();
-				break;
-			case paella.events.hideEditor:
-				if (paella.player.config.trimming && paella.player.config.trimming.enabled) {
-					paella.player.videoContainer.enableTrimming();
-				}
-				break;
-		}
-	},
+		getName() { return "es.upv.paella.trimmingPlayerPlugin"; }
+			
+		getEvents() { return [paella.events.controlBarLoaded, paella.events.showEditor, paella.events.hideEditor]; }
 	
-	loadTrimming:function() {
-		var videoId = paella.initDelegate.getId();
-		paella.data.read('trimming',{id:videoId},function(data,status) {
-			if (data && status && data.end>0) {
-				paella.player.videoContainer
-					.setTrimming(data.start, data.end)
-					.then(() => paella.player.videoContainer.enableTrimming() )
-				
+		onEvent(eventType,params) {
+			switch (eventType) {
+				case paella.events.controlBarLoaded:
+					this.loadTrimming();
+					break;
+				case paella.events.showEditor:
+					paella.player.videoContainer.disableTrimming();
+					break;
+				case paella.events.hideEditor:
+					if (paella.player.config.trimming && paella.player.config.trimming.enabled) {
+						paella.player.videoContainer.enableTrimming();
+					}
+					break;
 			}
-			else {
-				// Check for optional trim 'start' and 'end', in seconds, in location args
-				var startTime =  base.parameters.get('start');
-				var endTime = base.parameters.get('end');
-				if (startTime && endTime) {
+		}
+		
+		loadTrimming() {
+			var videoId = paella.initDelegate.getId();
+			paella.data.read('trimming',{id:videoId},function(data,status) {
+				if (data && status && data.end>0) {
 					paella.player.videoContainer
-						.setTrimming(startTime, endTime)
+						.setTrimming(data.start, data.end)
 						.then(() => paella.player.videoContainer.enableTrimming() )
+					
 				}
-			}
-		});
+				else {
+					// Check for optional trim 'start' and 'end', in seconds, in location args
+					var startTime =  base.parameters.get('start');
+					var endTime = base.parameters.get('end');
+					if (startTime && endTime) {
+						paella.player.videoContainer
+							.setTrimming(startTime, endTime)
+							.then(() => paella.player.videoContainer.enableTrimming() )
+					}
+				}
+			});
+		}
 	}
 });
-
-paella.plugins.trimmingLoaderPlugin = new paella.plugins.TrimmingLoaderPlugin();
