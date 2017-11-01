3f9fdeb2ead68260b9b3bc28f6ca0d1bdc5952bd
Karen Dolan <kdolan@dce.harvard.edu>
2017-11-01 13:27:01 -0400

    issues/291 Optional pause for onresize timer in PluginManager

--- src/05_plugin_base.js
+++ src/05_plugin_base.js
@@ -22,7 +22,7 @@
 		pluginList: [],
 		eventDrivenPlugins: [],
 		enabledPlugins: [],
-	
+		doResize: true,
 	
 	//	checkPluginVisibility
 	
@@ -51,7 +51,7 @@
 			});
 			
 			var timer = new base.Timer(function() {
-				if (paella.player && paella.player.controls) paella.player.controls.onresize();
+				if (paella.player && paella.player.controls && This.doResize) paella.player.controls.onresize();
 			}, 1000);
 			timer.repeat = true;
 		},
