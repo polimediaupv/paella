b2e33f7b4472cd1ee688184172a0579e73d0536e
Miguel Escriva <miesgre@gmail.com>
2018-01-11 09:35:25 +0100

    travis deploy scripts

---
+++ .publish_scripts/bower/publish.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+SCRIPT_FOLDER=$(dirname $0)
+SCRIPT_PARENT_FOLDER=$(dirname $SCRIPT_FOLDER)
+SCRIPT_FILENAME="$(basename "$0")"
+
+source  $SCRIPT_PARENT_FOLDER/utility.sh
+#. _scripts/github.sh
+
+usage() {
+    echo "usage: $0 -d"
+    echo "                  -v VERSION"
+    echo "                 [-f FOLDER]"
+    echo "                 [-r GITHUB_BOWER_REPO]"
+    echo "                 [-c GITHUB_BOWER_TOKEN]"
+    echo "                 [-n GIT_BOWER_NAME]"
+    echo "                 [-e GIT_BOWER_EMAIL]"
+    echo
+}
+
+## MAIN
+DEPLOY_FOLDER="."
+GIT_BOWER_EMAIL="travis@travis-ci.org"
+GIT_BOWER_NAME="Travis CI"
+
+while getopts ":du:v:f:r:c:n:e:" opt; do
+    case $opt in
+        d)
+            DEBUG=1
+            ;;
+        v)
+            VERSION="$OPTARG"
+            ;;
+        f)
+            DEPLOY_FOLDER="$OPTARG"
+            ;;            
+        r)
+            GIT_BOWER_REPO="$OPTARG"
+            ;;
+        c)
+            GIT_BOWER_TOKEN="$OPTARG"
+            ;;
+        n)
+            GIT_BOWER_NAME="$OPTARG"
+            ;;
+        e)
+            GIT_BOWER_EMAIL="$OPTARG"
+            ;;
+        \?)
+            set +x
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            set +x
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ "${DEBUG:-}" == "1" ]; then
+  echo "[INFO] Debug Enabled"
+  set -x
+fi
+
+set -e
+
+if [ "${VERSION}x" == "x" ]; then
+  echo "[ERROR] No version defined"
+  usage
+  exit 1
+fi
+
+if [ "${DEPLOY_FOLDER}x" == "x" ]; then
+  echo "[ERROR] No deploy folder defined"
+  usage
+  exit 1
+fi
+
+if [ ! -d "${DEPLOY_FOLDER}" ]; then
+  echo "[ERROR] Deploy folder does not exists"  
+  exit 1
+fi
+
+if [ "${GIT_BOWER_REPO}x" == "x" ] ; then
+  echo "[ERROR] No git repo for bower defined"
+  exit 1
+fi
+
+if [ "${GIT_BOWER_TOKEN}x" == "x" ] ; then
+  echo "[ERROR] No git token for bower defined"
+  exit 1
+fi
+
+
+ret=$( is_proper_version $VERSION )
+if [[ ! 0 == $ret ]]; then # it is a proper semver VERSION
+  echo "[INFO] Nothing published as version $VERSION is not a proper version format"
+  exit 0
+fi
+
+
+echo "[INFO] Publishing bower package version=$VERSION"
+
+TMP_FOLDER=$(mktemp -d -t deploy-npm)
+echo "[INFO] Using $TMP_FOLDER as temporal folder"
+
+git clone git://github.com/$GIT_BOWER_REPO $TMP_FOLDER
+rm -rf $TMP_FOLDER/*
+
+cp -r $DEPLOY_FOLDER/ $TMP_FOLDER
+
+if [ -f "$SCRIPT_FOLDER/bower.json" ]; then
+  cp $SCRIPT_FOLDER/bower.json $TMP_FOLDER
+fi
+if [ -f "$SCRIPT_FOLDER/README.md" ]; then
+  cp $SCRIPT_FOLDER/README.md $TMP_FOLDER
+fi
+
+
+pushd $TMP_FOLDER > /dev/null
+
+echo "[INFO] Setup git name=$GIT_BOWER_NAME email=$GIT_BOWER_EMAIL"
+git config --local user.email "$GIT_BOWER_EMAIL"
+git config --local user.name "$GIT_BOWER_NAME"
+
+git add -A
+git commit -q -m "[automated publishing] version $VERSION" 
+git tag $VERSION
+git push --tags https://${GIT_BOWER_TOKEN}@github.com/${GIT_BOWER_REPO} master
+
+popd > /dev/null
+
+echo "[INFO] Removing remporal folder"
+rm -rf $TMP_FOLDER
+echo "[INFO] Done!"
---
+++ .publish_scripts/npm/publish.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+SCRIPT_FOLDER=$(dirname $0)
+SCRIPT_PARENT_FOLDER=$(dirname $SCRIPT_FOLDER)
+SCRIPT_FILENAME="$(basename "$0")"
+
+source  $SCRIPT_PARENT_FOLDER/utility.sh
+#. _scripts/github.sh
+
+usage() {
+    echo "usage: $0 -d"
+    echo "                  -v VERSION"
+    echo "                 [-f FOLDER]"
+    echo "                 [-c NPM_TOKEN]"
+    echo "                 [-t NPM_TAG]"
+    echo
+}
+
+## MAIN
+DEPLOY_FOLDER="."
+NPM_TAG="latest"
+
+
+while getopts ":du:v:f:t:c:" opt; do
+    case $opt in
+        d)
+            DEBUG=1
+            ;;
+        v)
+            VERSION="$OPTARG"
+            ;;
+        f)
+            DEPLOY_FOLDER="$OPTARG"
+            ;;            
+        t)
+            NPM_TAG="$OPTARG"
+            ;;
+        c)
+            NPM_TOKEN="$OPTARG"
+            ;;
+        \?)
+            set +x
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            set +x
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ "${DEBUG:-}" == "1" ]; then
+  echo "[INFO] Debug Enabled"
+  set -x
+fi
+
+set -e
+
+if [ "${VERSION}x" == "x" ]; then
+  echo "[ERROR] No version defined"
+  usage
+  exit 1
+fi
+
+if [ "${DEPLOY_FOLDER}x" == "x" ]; then
+  echo "[ERROR] No deploy folder defined"
+  usage
+  exit 1
+fi
+
+if [ ! -d "${DEPLOY_FOLDER}" ]; then
+  echo "[ERROR] Deploy folder does not exists"  
+  exit 1
+fi
+
+if [ "${NPM_TOKEN}x" == "x" ] ; then
+  echo "[ERROR] No npm token defined"
+  usage
+  exit 1
+fi
+
+
+
+ret=$( is_proper_version $VERSION )
+if [[ ! 0 == $ret ]]; then # it is a proper semver VERSION
+  echo "[INFO] Nothing published as version $VERSION is not a proper version format"
+  exit 0
+fi
+
+TMP_FOLDER=$(mktemp -d -t deploy-npm)
+echo "[INFO] Using $TMP_FOLDER as temporal folder"
+
+cp -r $DEPLOY_FOLDER/ $TMP_FOLDER
+
+if [ -f "$SCRIPT_FOLDER/package.json" ]; then
+  cp $SCRIPT_FOLDER/package.json $TMP_FOLDER
+fi
+if [ -f "$SCRIPT_FOLDER/README.md" ]; then
+  cp $SCRIPT_FOLDER/README.md $TMP_FOLDER
+fi
+replaceJsonProp "$TMP_FOLDER/package.json" "version" ".*" "$VERSION"
+
+cat > "$TMP_FOLDER/.npmrc" <<EOF
+//registry.npmjs.org/:_authToken=$NPM_TOKEN
+EOF
+cat > "$TMP_FOLDER/.npmignore" <<EOF
+.npmrc
+EOF
+
+pushd $TMP_FOLDER > /dev/null
+echo "[INFO] Publishing npm package version=$VERSION tag=$NPM_TAG"
+npm publish --tag $NPM_TAG
+popd > /dev/null
+
+echo "[INFO] Removing remporal folder"
+rm -rf $TMP_FOLDER
+echo "[INFO] Done!"
