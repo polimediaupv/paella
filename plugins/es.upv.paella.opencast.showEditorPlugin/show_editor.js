939b27f5460ebe44a753539190ce918238452760
Miguel Escriva <miesgre@gmail.com>
2018-09-05 14:48:08 +0200

    new Plugin: es.upv.paella.showEditorPlugin

---
+++ plugins/es.upv.paella.opencast.showEditorPlugin/show_editor.js
@@ -0,0 +1,32 @@
+paella.addPlugin(function() {
+	return class ShowEditorPlugin extends paella.VideoOverlayButtonPlugin {
+		getName() {
+			return "es.upv.paella.showEditorPlugin";
+		}
+		getSubclass() { return "showEditorButton"; }
+		getIconClass() { return 'icon-pencil'; }
+		getAlignment() { return 'right'; }
+		getIndex() {return 10;}
+		getDefaultToolTip() { return base.dictionary.translate("Enter editor mode"); }
+
+		checkEnabled(onSuccess) {
+			console.log("ii");
+			if (this.config.editorUrl) {
+				paella.initDelegate.initParams.accessControl.canWrite()
+				.then((canWrite)=>{
+					var enabled = (canWrite); // && !base.userAgent.browser.IsMobileVersion && !paella.player.isLiveStream());					
+					onSuccess(enabled);
+				});	
+			}
+			else {				
+				onSuccess(false);
+			}
+		}
+
+		action(button) {
+			var editorUrl = this.config.editorUrl.replace("{id}", paella.player.videoIdentifier);
+			window.location.href = editorUrl;
+		}
+	}
+});
+
