ce090e8171aa04a1cf58b890b561c95d6502aef8
Florian Feyen <ff@flause.io>
2019-01-09 12:56:56 +0100

    import changes from paella 5.3.10 version

--- plugins/org.opencast.usertracking.x5gonSaverPlugIn/usertracking_x5gon_saver.js
+++ plugins/org.opencast.usertracking.x5gonSaverPlugIn/usertracking_x5gon_saver.js
@@ -1,14 +1,13 @@
-paella.addPlugin(function () {
+paella.addPlugin(function() {
     return class X5gonTracking extends paella.userTracking.SaverPlugIn {
-        getName() {
-            return "org.opencast.usertracking.x5gonSaverPlugIn";
-        }
+        getName() { 
+            return "org.opencast.usertracking.x5gonSaverPlugIn"; 
+        };
 
         checkEnabled(onSuccess) {
-            console.log("test");
-
+            
             /* don't change these variables */
-            var token = this.config.trackingID,
+            var token = this.config.token,
                 mediapackageError = false,
                 allow_tracking = true,
                 path,
@@ -25,33 +24,38 @@ paella.addPlugin(function () {
                     return true;
                 }
                 return false;
-            };
+            }
 
-            //trackX5gon();
-
-            function trackX5gon(onSuccess) {
+            function trackX5gon() {
+                console.log("X5gon: trackX5gon method call");
                 if (isTrackingPermission() && !tracked) {
                     if (!token) {
-                        base.log("X5gon: token missing! Disabling X5gon PlugIn");
+                        base.log.debug("X5gon: token missing! Disabling X5gon PlugIn");
                         onSuccess(false);
-                    } else {
+                        }
+                    else {
+                        // load x5gon lib from remote server
+                        console.log("X5gon: trackX5gon loading x5gon-snippet")
+                        require(["https://platform.x5gon.org/api/v1/snippet/latest/x5gon-log.min.js"], function (x5gon) {
+                            base.log.debug("X5gon: external x5gon snippet loaded");
+                            console.log("X5gon: external x5gon snippet loaded");
+                        });            
                         //x5gonActivityTracker('6j3hfn', testingEnvironment);
                         tracked = true;
-                        base.log("X5gon: send data to X5gon servers");
+                        base.log.debug("X5gon: send data to X5gon servers");
                         onSuccess(true);
                     }
                 }
-            };
+            }
 
             function isTrackingPermission() {
-                if (isDoNotTrackStatus()) return false;
-                if (trackingPermission === undefined) {
-                    trackingPermission = storedConsent;
-                    if (trackingPermission === undefined) {
-                        return false;
-                    }
-                }
-                return trackingPermission;
+                if (isDoNotTrackStatus()) {
+                    console.log("X5gon: trackingPermissions: false");
+                    return false;
+                } else {
+                    console.log("X5gon: trackingPermissions: true");
+                    return true;   
+                }  
             }
 
             function setTrackingPermission(status) {
@@ -60,7 +64,10 @@ paella.addPlugin(function () {
                 //trackingPermission = status;
                 //trackX5gon(); 
             };
-        }
+
+            trackX5gon();
+            onSuccess(true);
+        };
 
         log(event, params) {
             if ((this.config.category === undefined) || (this.config.category === true)) {
@@ -72,6 +79,6 @@ paella.addPlugin(function () {
                     label = JSON.stringify(params);
                 } catch (e) {}
             }
-        }
-    }
+        };
+    };
 });
