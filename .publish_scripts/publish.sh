b269fb7513c8ecd67ee80134d8b186ce0670b252
Miguel Escriva <miesgre@gmail.com>
2019-01-29 12:29:25 +0100

    travis scripts

--- .publish_scripts/publish.sh
+++ .publish_scripts/publish.sh
@@ -9,14 +9,37 @@ SCRIPT_FILENAME="$(basename "$0")"
 TGZ_OUTPUT=$HOME/prebuilt
 
 GITHUB_BOWER_REPO="polimediaupv/bower-paella"
+GITHUB_WEBPAGE_REPO="polimediaupv/paellaplayer.upv.es"
 POLIMEDIA_NAME="Polimedia Team"
 POLIMEDIA_EMAIL="polim@upvnet.upv.es"
 
-VERSION=${TRAVIS_TAG}
+
 DEPLOY_FOLDER=`pwd`/build/
 
+if [ ! "${TRAVIS_PULL_REQUEST}x" == "falsex" ]; then
+	echo "[ERROR] This is a pull request. You can not publish a PR"
+	exit 1
+fi
+
 if [ ! -z "${TRAVIS_TAG}" ]; then
+	echo "[INFO] Publishing on TAG ${TRAVIS_TAG}"
+	VERSION=${TRAVIS_TAG}
+	./$SCRIPT_FOLDER/webpage/publish_player.sh -v $VERSION -f `pwd` -r $GITHUB_WEBPAGE_REPO -n "$POLIMEDIA_NAME" -e "$POLIMEDIA_EMAIL" -c $GITHUB_AUTH_TOKEN
 	./$SCRIPT_FOLDER/npm/publish.sh -v $VERSION -c $NPM_TOKEN -f "$DEPLOY_FOLDER" 
 	./$SCRIPT_FOLDER/bower/publish.sh -v $VERSION -f "$DEPLOY_FOLDER" -r $GITHUB_BOWER_REPO -n "$POLIMEDIA_NAME" -e "$POLIMEDIA_EMAIL" -c $GITHUB_AUTH_TOKEN
 	./$SCRIPT_FOLDER/tarball/publish.sh -v $VERSION -f "$DEPLOY_FOLDER" -o $TGZ_OUTPUT
+elif [ ! -z "${TRAVIS_BRANCH}" ]; then
+	echo "[INFO] Commit on branch ${TRAVIS_BRANCH} (${TRAVIS_COMMIT})"
+    if [[ "$TRAVIS_BRANCH" =~ ^([0-9]+\.[0-9]+\.x)$ ]] ; then
+        VERSION=${BASH_REMATCH[1]}        
+    elif [[ "${TRAVIS_BRANCH}x" == "developx" ]] ; then
+        VERSION=develop
+    fi
+	if [ -z "${VERSION}" ]; then
+		echo "[ERROR] No publication for branch ${TRAVIS_BRANCH}"
+	else
+		echo "[INFO] Publishing on branch ${TRAVIS_BRANCH} (VERSION=${VERSION})"
+		echo ./$SCRIPT_FOLDER/webpage/publish_player.sh -v $VERSION -f `pwd` -r $GITHUB_WEBPAGE_REPO -n "$POLIMEDIA_NAME" -e "$POLIMEDIA_EMAIL" -c $GITHUB_AUTH_TOKEN -y
+		echo ./$SCRIPT_FOLDER/webpage/publish_doc.sh -v $VERSION -f `pwd` -r $GITHUB_WEBPAGE_REPO -n "$POLIMEDIA_NAME" -e "$POLIMEDIA_EMAIL" -c $GITHUB_AUTH_TOKEN
+	fi
 fi
---
+++ .publish_scripts/webpage/publish_doc.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+SCRIPT_FOLDER=$(dirname $0)
+SCRIPT_PARENT_FOLDER=$(dirname $SCRIPT_FOLDER)
+SCRIPT_FILENAME="$(basename "$0")"
+
+source  $SCRIPT_PARENT_FOLDER/utility.sh
+#. _scripts/github.sh
+
+usage() {
+    echo "usage: $0 -d"
+    echo "                  -v VERSION"
+    echo "                 [-f SRC_PAELLA_FOLDER]"
+    echo "                 [-r GITHUB_WEBPAGE_REPO]"
+    echo "                 [-c GITHUB_WEBPAGE_TOKEN]"
+    echo "                 [-n GIT_NAME]"
+    echo "                 [-e GIT_EMAIL]"
+    echo
+}
+
+## MAIN
+SRC_PAELLA_FOLDER="."
+GIT_EMAIL="travis@travis-ci.org"
+GIT_NAME="Travis CI"
+
+while getopts ":du:v:f:r:c:n:e:" opt; do
+    case $opt in
+        d)
+            DEBUG=1
+            ;;
+        v)
+            VERSION="$OPTARG"
+            ;;
+        f)
+            SRC_PAELLA_FOLDER="$OPTARG"
+            ;;            
+        r)
+            GITHUB_WEBPAGE_REPO="$OPTARG"
+            ;;
+        c)
+            GITHUB_WEBPAGE_TOKEN="$OPTARG"
+            ;;
+        n)
+            GIT_NAME="$OPTARG"
+            ;;
+        e)
+            GIT_EMAIL="$OPTARG"
+            ;;
+        \?)
+            set +x
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            set +x
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if [ "${DEBUG:-}" == "1" ]; then
+  echo "[INFO] Debug Enabled"
+  set -x
+fi
+
+set -e
+
+if [ "${VERSION}x" == "x" ]; then
+  echo "[ERROR] No version defined"
+  usage
+  exit 1
+fi
+
+if [ "${SRC_PAELLA_FOLDER}x" == "x" ]; then
+  echo "[ERROR] No source paella folder defined"
+  usage
+  exit 1
+fi
+
+if [ ! -d "${SRC_PAELLA_FOLDER}" ]; then
+  echo "[ERROR] Source paella folder does not exists"  
+  exit 1
+fi
+
+if [ "${GITHUB_WEBPAGE_REPO}x" == "x" ] ; then
+  echo "[ERROR] No git repo for webpage defined"
+  exit 1
+fi
+
+if [ "${GITHUB_WEBPAGE_TOKEN}x" == "x" ] ; then
+  echo "[ERROR] No git token defined"
+  exit 1
+fi
+
+
+
+echo "[INFO] Publishing documentation for version=$VERSION"
+
+TMP_FOLDER=$(mktemp -d -t deploy-doc.XXXXXX)
+echo "[INFO] Using $TMP_FOLDER as temporal folder"
+
+git clone https://${GITHUB_WEBPAGE_TOKEN}@github.com/${GITHUB_WEBPAGE_REPO}.git --branch gh-pages $TMP_FOLDER
+
+
+if [ -f "${SRC_PAELLA_FOLDER}/doc/toc.yml" ] ; then
+
+    pushd $TMP_FOLDER > /dev/null
+
+    echo "[INFO] Copy documentation for version=${VERSION}"
+    if [ -d "_docs/${VERSION}" ] ; then
+        git rm -r _docs/${VERSION}/*
+    fi
+    cp -r ${SRC_PAELLA_FOLDER}/doc ${TMP_FOLDER}/_docs/${VERSION}
+    mv ${TMP_FOLDER}/_docs/${VERSION}/toc.yml ${TMP_FOLDER}/_data/docs/${VERSION}.yml
+    if [ ! -f "${SRC_PAELLA_FOLDER}/doc/changelog.md" ] ; then 
+        echo -e  "---\n---\n\n" >${TMP_FOLDER}/_docs/${VERSION}/changelog.md
+        cat ${SRC_PAELLA_FOLDER}/CHANGELOG >> ${TMP_FOLDER}/_docs/${VERSION}/changelog.md
+    fi
+
+    echo "[INFO] Setup git name=$GIT_NAME email=$GIT_EMAIL"
+    git config --local user.email "$GIT_EMAIL"
+    git config --local user.name "$GIT_NAME"
+
+    git add ./*
+    git status
+    git commit -m "Update documentation for version ${VERSION}"
+    
+    git push https://${GITHUB_WEBPAGE_TOKEN}@github.com/${GITHUB_WEBPAGE_REPO}.git gh-pages
+    popd > /dev/null
+fi
+
+echo "[INFO] Removing remporal folder"
+rm -rf $TMP_FOLDER
+echo "[INFO] Done!"
