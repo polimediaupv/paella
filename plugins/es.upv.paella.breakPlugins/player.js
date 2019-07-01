9aff2203042c1ee7421ffce5da818eb5abbfbfd7
Fernando Serrano Carpena <ferserc1@gmail.com>
2019-07-01 11:36:03 +0200

    Bug fixed in breaks plugin

--- plugins/es.upv.paella.breakPlugins/player.js
+++ plugins/es.upv.paella.breakPlugins/player.js
@@ -1,132 +1,91 @@
 paella.addPlugin(() => {
 	return class BreaksPlayerPlugin extends paella.EventDrivenPlugin {
-		get breaks() {
-			return this._breaks || []
-		}
-		set breaks(b) {
-			this._breaks = b;
-		}
-		get lastEvent() {
-			return this._lastEvent || 0;
-		}
-		set lastEvent(e) {
-			this._lastEvent = e;
-		}
-		get visibleBreaks() {
-			return this._visibleBreaks || [];
-		}
-		set visibleBreaks(v) {
-			this._visibleBreaks = v;
+		getName() { return "es.upv.paella.breaksPlayerPlugin"; }
+
+		checkEnabled(onSuccess) {
+			onSuccess(true);
 		}
 
-		getName() {
-			return "es.upv.paella.breaksPlayerPlugin";
-		}
-		checkEnabled(onSuccess) {
-			var This = this;
-			paella.data.read('breaks', {
-				id: paella.initDelegate.getId()
-			}, function (data, status) {
+		setup() {
+			this.breaks = [];
+			this.status = false;
+			this.lastTime = 0;
+			paella.data.read('breaks', { id: paella.player.videoIdentifier }, (data) => {
 				if (data && typeof (data) == 'object' && data.breaks && data.breaks.length > 0) {
-					This.breaks = data.breaks;
+					this.breaks = data.breaks;
 				}
-				onSuccess(true);
 			});
 		}
 
-		getEvents() {
-			return [paella.events.timeUpdate];
-		}
+		getEvents() { return [ paella.events.timeUpdate ]; }
 
 		onEvent(eventType, params) {
-			var thisClass = this;
-
-			params.videoContainer.currentTime(true)
-				.then(function (currentTime) {
-					thisClass.checkBreaks(currentTime);
+			paella.player.videoContainer.currentTime(true)
+				.then((currentTime) => {
+					// The event type checking must to be done using the time difference, because
+					// the timeUpdate event may arrive before the seekToTime event
+					let diff = Math.abs(currentTime - this.lastTime);
+					this.checkBreaks(currentTime,diff>=1 ? paella.events.seekToTime : paella.events.timeUpdate);
+					this.lastTime = currentTime;
 				});
 		}
 
-		checkBreaks(currentTime) {
-			var a;
-			for (var i = 0; i < this.breaks.length; ++i) {
-				a = this.breaks[i];
-
-				if (a.s < currentTime && a.e > currentTime) {
-					if (this.areBreaksClickable())
-						this.avoidBreak(a);
-					else
-						this.showBreaks(a);
-				} else if (a.s.toFixed(0) == currentTime.toFixed(0)) {
-					this.avoidBreak(a);
-				}
-			}
-			if (!this.areBreaksClickable()) {
-				for (var key in this.visibleBreaks) {
-					if (typeof (a) == 'object') {
-						a = this.visibleBreaks[key];
-						if (a && (a.s >= currentTime || a.e <= currentTime)) {
-							this.removeBreak(a);
-						}
+		checkBreaks(currentTime,eventType) {
+			let breakMessage = "";
+			if (this.breaks.some((breakItem) => {
+				if (breakItem.s<=currentTime && breakItem.e>=currentTime) {
+					if (eventType==paella.events.timeUpdate && !this.status) {
+						this.skipTo(breakItem.e);
 					}
+					breakMessage = breakItem.text;
+					return true;
 				}
+			})) {
+				this.showMessage(breakMessage);
+				this.status = true;
+			}
+			else {
+				this.hideMessage();
+				this.status = false;
 			}
 		}
 
-		areBreaksClickable() {
-			//Returns true if the config value is set and if we are not on the editor.
-			return this.config.neverShow && !(paella.editor.instance && paella.editor.instance.isLoaded);
+		skipTo(time) {
+			paella.player.videoContainer.trimming()
+				.then((trimming) => {
+					if (trimming.enabled) {
+						paella.player.videoContainer.seekToTime(time + trimming.start);
+					}
+					else {
+						paella.player.videoContainer.seekToTime(time);
+					}
+				})
 		}
 
-		showBreaks(br) {
-			if (!this.visibleBreaks[br.s]) {
+		showMessage(text) {
+			if (this.currentText != text) {
+				if (this.messageContainer) {
+					paella.player.videoContainer.overlayContainer.removeElement(this.messageContainer);
+				}
 				var rect = {
 					left: 100,
 					top: 350,
 					width: 1080,
 					height: 40
 				};
-				let name = br.name || paella.dictionary.translate("Break")
-				br.elem = paella.player.videoContainer.overlayContainer.addText(name, rect);
-				br.elem.className = 'textBreak';
-				this.visibleBreaks[br.s] = br;
+				this.currentText = text;
+				this.messageContainer = paella.player.videoContainer.overlayContainer.addText(paella.dictionary.translate(text), rect);
+				this.messageContainer.className = 'textBreak';
+				this.currentText = text;
 			}
 		}
 
-		removeBreak(br) {
-			if (this.visibleBreaks[br.s]) {
-				var elem = this.visibleBreaks[br.s].elem;
-				paella.player.videoContainer.overlayContainer.removeElement(elem);
-				this.visibleBreaks[br.s] = null;
+		hideMessage() {
+			if (this.messageContainer) {
+				paella.player.videoContainer.overlayContainer.removeElement(this.messageContainer);
+				this.messageContainer = null;
 			}
-		}
-
-		avoidBreak(br) {
-			var newTime;
-			if (paella.player.videoContainer.trimEnabled()) {
-				paella.player.videoContainer.trimming()
-					.then((trimming) => {
-						if (br.e >= trimming.end) {
-							newTime = 0;
-							paella.player.videoContainer.pause();
-						} else {
-							newTime = br.e + (this.config.neverShow ? 0.5 : 0) - trimming.start;
-						}
-						paella.player.videoContainer.seekToTime(newTime);
-
-					});
-			} else {
-				paella.player.videoContainer.duration(true)
-					.then((duration) => {
-						if (br.e >= duration) {
-							newTime = 0;
-							paella.player.videoContainer.pause();
-						} else {
-							newTime = br.e + (this.config.neverShow ? 0.5 : 0);
-						}
-						paella.player.videoContainer.seekToTime(newTime);
-					});
-			}
+			this.currentText = "";
 		}
 	}
 });
