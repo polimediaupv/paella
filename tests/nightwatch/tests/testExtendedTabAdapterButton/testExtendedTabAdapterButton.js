3cb349de4e2eda53983fd1b87ba2608a95fdb4fb
mhaba <mhc93.marcos@gmail.com>
2019-11-22 09:48:10 +0100

    Extended tab adapter button first approach test.

---
+++ tests/nightwatch/tests/testExtendedTabAdapterButton/testExtendedTabAdapterButton.js
@@ -0,0 +1,19 @@
+module.exports = {
+    'Test Page Load' : function (browser) {
+      browser
+        .url("http://localhost:8000/player/index.html?id=video-remote-1")
+        .waitForElementVisible('body', 1000)
+        .assert.visible('.play-icon')
+        .click('.videoWrapper')
+        .waitForElementNotVisible('.play-icon')
+
+        .waitForElementVisible('#buttonPlugin14')
+        .assert.visible('#buttonPlugin14')
+
+        .click('#buttonPlugin14')
+        .waitForElementVisible('.tabsContentContainer')
+        .assert.visible('.tabsContentContainer')
+
+        .end();
+    }
+  };
