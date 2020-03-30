8441a7628e7deb369fc1bf2e8657635dd521cb0f
Fernando Serrano Carpena <ferserc1@gmail.com>
2020-03-30 15:20:10 +0200

    HLS multi audio now works with audio selector plugin.

--- plugins/es.upv.paella.audioSelector/index.js
+++ plugins/es.upv.paella.audioSelector/index.js
@@ -10,49 +10,97 @@ paella.addPlugin(function() {
 		closeOnMouseOut() { return true; }
 			
 		checkEnabled(onSuccess) {
-			paella.player.videoContainer.getAudioTags()
-				.then((tags) => {
-					this._tags = tags;
-					onSuccess(tags.length>1);
-				});
+			this._mainPlayer = paella.player.videoContainer.streamProvider.mainVideoPlayer;
+			this._mainPlayer.supportsMultiaudio()
+				.then((supports)=> {
+					if (supports) {
+						this._legacyMode = false;
+						return this._mainPlayer.getAudioTracks();
+					}
+					else {
+						this._legacyMode = true;
+						return paella.player.videoContainer.getAudioTags();
+					}
+				})
+
+				.then((audioTracks) => {
+					if (this._legacyMode) {
+						this._tags = audioTracks;
+						return Promise.resolve();
+					}
+					else {
+						this._audioTracks = audioTracks;
+						return this._mainPlayer.getCurrentAudioTrack();
+					}
+				})
+
+				.then((defaultAudioTrack) => {
+					if (this._legacyMode) {
+						onSuccess(this._tags.length>1);
+					}
+					else {
+						this._defaultAudioTrack = defaultAudioTrack;
+						onSuccess(true);
+					}
+				})
 		}
 
 		getButtonType() { return paella.ButtonPlugin.type.menuButton; }
 
-		setup() {
-			
-			
-		}
-
 		getMenuContent() {
 			let buttonItems = [];
 
-			this._tags.forEach((tag,index) => {
-				buttonItems.push({
-					id: index,
-					title: tag,
-					value: tag,
-					icon: "",
-					className: this.getButtonItemClass(tag),
-					default: tag == paella.player.videoContainer.audioTag
+			if (this._legacyMode) {
+				this._tags.forEach((tag,index) => {
+					buttonItems.push({
+						id: index,
+						title: tag,
+						value: tag,
+						icon: "",
+						className: this.getButtonItemClass(tag),
+						default: tag == paella.player.videoContainer.audioTag
+					});
 				});
-			});
+			}
+			else {
+				this._audioTracks.forEach((track) => {
+					buttonItems.push({
+						id: track.id,
+						title: track.groupId + " " + track.name,
+						value: track.id,
+						icon: "",
+						className: this.getButtonItemClass(track.id),
+						default: track.id == this._defaultAudioTrack.id
+					});
+				});
+			}
 
 			return buttonItems;
 		}
 
 		menuItemSelected(itemData) {
-			paella.player.videoContainer.setAudioTag(itemData.value);
+			if (this._legacyMode) {
+				paella.player.videoContainer.setAudioTag(itemData.value);
+			}
+			else {
+				this._mainPlayer.setCurrentAudioTrack(itemData.id);
+			}
+			paella.player.controls.hidePopUp(this.getName());
 		}
 		
 		setQualityLabel() {
-			var This = this;
-			paella.player.videoContainer.getCurrentQuality()
-				.then(function(q) {
-					This.setText(q.shortLabel());
-				});
+			if (this._legacyMode) {
+				var This = this;
+				paella.player.videoContainer.getCurrentQuality()
+					.then(function(q) {
+						This.setText(q.shortLabel());
+					});
+			}
+			else {
+
+			}
 		}
-		
+
 		getButtonItemClass(tag) {
 			return 'videoAudioTrackItem ' + tag;
 		}
