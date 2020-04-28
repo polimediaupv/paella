ae3aa0d594927cd3644613df6bf88a4b7d0789cc
Fernando Serrano Carpena <ferserc1@gmail.com>
2020-04-28 08:48:10 +0200

    Fixed a bug caused by the refactoring of the timerManager

--- src/01_utils.js
+++ src/01_utils.js
@@ -280,9 +280,9 @@ paella.utils.objectFromString = (str) => {
 			timer.timerId = this.lastId;
 			timer.timeout = time;
 			this.timerArray[this.lastId] = timer;
-			timer.jsTimerId = setTimeout(() => {
-				timerManager.executeTimerCallback(this.lastId);	
-			}, time);
+			timer.jsTimerId = setTimeout(
+				`g_timerManager.executeTimerCallback(${ this.lastId })`	
+			, time);
 		}
 	
 		executeTimerCallback(timerId) {
@@ -291,14 +291,14 @@ paella.utils.objectFromString = (str) => {
 				timer.callback(timer,timer.params);
 			}
 			if (timer.repeat) {
-				timer.jsTimerId = setTimeout(() => {
-					timerManager.executeTimerCallback(timer.timerId);
-				}, timer.timeout);
+				timer.jsTimerId = setTimeout(
+					`g_timerManager.executeTimerCallback(${ timer.timerId })`
+				, timer.timeout);
 			}
 		}
 	}
 
-	const timerManager = new TimerManager();
+	window.g_timerManager = new TimerManager();
 
 	class Timer {
 		static sleep(milliseconds) {
@@ -314,7 +314,7 @@ paella.utils.objectFromString = (str) => {
 			this.callback = callback;
 			this.params = params;
 			this._repeat = false;
-			timerManager.setupTimer(this,time);
+			g_timerManager.setupTimer(this,time);
 		}
 
 		get repeat() { return this._repeat; }
@@ -327,177 +327,6 @@ paella.utils.objectFromString = (str) => {
 
 	paella.utils.Timer = Timer;
 })();
-
-(function() {
-	let g_delegateCallbacks = {};
-	let g_dataDelegates = [];
-
-	class DataDelegate {
-		read(context,params,onSuccess) {
-			if (typeof(onSuccess)=='function') {
-				onSuccess({},true);
-			}
-		}
-
-		write(context,params,value,onSuccess) {
-			if (typeof(onSuccess)=='function') {
-				onSuccess({},true);
-			}
-		}
-
-		remove(context,params,onSuccess) {
-			if (typeof(onSuccess)=='function') {
-				onSuccess({},true);
-			}
-		}
-	}
-
-	paella.DataDelegate = DataDelegate;
-
-	paella.dataDelegates = {};
-
-	class Data {
-		get enabled() { return this._enabled; }
-
-		get dataDelegates() { return g_dataDelegates; }
-	
-		constructor(config) {
-			this._enabled = config.data.enabled;
-
-			// Delegate callbacks
-			let executedCallbacks = [];
-			for (let context in g_delegateCallbacks) {
-				let callback = g_delegateCallbacks[context];
-				let DelegateClass = null;
-				let delegateName = null;
-
-				if (!executedCallbacks.some((execCallbackData) => {
-					if (execCallbackData.callback==callback) {
-						delegateName = execCallbackData.delegateName;
-						return true;
-					}
-				})) {
-					DelegateClass = g_delegateCallbacks[context]();
-					delegateName = DelegateClass.name;
-					paella.dataDelegates[delegateName] = DelegateClass;
-					executedCallbacks.push({ callback:callback, delegateName:delegateName });
-				}
-
-				if (!config.data.dataDelegates[context]) {
-					config.data.dataDelegates[context] = delegateName;
-				}
-
-			}
-
-			for (var key in config.data.dataDelegates) {
-				try {
-					
-					var delegateName = config.data.dataDelegates[key];
-					var DelegateClass = paella.dataDelegates[delegateName];
-					var delegateInstance = new DelegateClass();
-					g_dataDelegates[key] = delegateInstance;
-				
-				}
-				catch (e) {
-					console.warn("Warning: delegate not found - " + delegateName);
-				}
-			}
-
-
-			// Default data delegate
-			if (!this.dataDelegates["default"]) {
-				this.dataDelegates["default"] = new paella.dataDelegates.DefaultDataDelegate();
-			}
-		}
-	
-		read(context,key,onSuccess) {
-			var del = this.getDelegate(context);
-			del.read(context,key,onSuccess);
-		}
-	
-		write(context,key,params,onSuccess) {
-			var del = this.getDelegate(context);
-			del.write(context,key,params,onSuccess);
-		}
-	
-		remove(context,key,onSuccess) {
-			var del = this.getDelegate(context);
-			del.remove(context,key,onSuccess);
-		}
-	
-		getDelegate(context) {
-			if (this.dataDelegates[context]) return this.dataDelegates[context];
-			else return this.dataDelegates["default"];
-		}
-	}
-
-	paella.Data = Data;
-
-	paella.addDataDelegate = function(context,callback) {
-		if (Array.isArray(context)) {
-			context.forEach((ctx) => {
-				g_delegateCallbacks[ctx] = callback;
-			})
-		}
-		else if (typeof(context)=="string") {
-			g_delegateCallbacks[context] = callback;
-		}
-	}
-
-})();
-
-paella.addDataDelegate(["default","trimming"], () => {
-	paella.dataDelegates.DefaultDataDelegate = class CookieDataDelegate extends paella.DataDelegate {
-		serializeKey(context,params) {
-			if (typeof(params)=='object') {
-				params = JSON.stringify(params);
-			}
-			return context + '|' + params;
-		}
-	
-		read(context,params,onSuccess) {
-			var key = this.serializeKey(context,params);
-			var value = base.cookies.get(key);
-			try {
-				value = unescape(value);
-				value = JSON.parse(value);
-			}
-			catch (e) {}
-			if (typeof(onSuccess)=='function') {
-				onSuccess(value,true);
-			}
-		}
-	
-		write(context,params,value,onSuccess) {
-			var key = this.serializeKey(context,params);
-			if (typeof(value)=='object') {
-				value = JSON.stringify(value);
-			}
-			value = escape(value);
-			base.cookies.set(key,value);
-			if(typeof(onSuccess)=='function') {
-				onSuccess({},true);
-			}
-		}
-	
-		remove(context,params,onSuccess) {
-			var key = this.serializeKey(context,params);
-			if (typeof(value)=='object') {
-				value = JSON.stringify(value);
-			}
-			base.cookies.set(key,'');
-			if(typeof(onSuccess)=='function') {
-				onSuccess({},true);
-			}
-		}
-	}
-
-	return paella.dataDelegates.DefaultDataDelegate;
-})
-
-// Will be initialized inmediately after loading config.json, in PaellaPlayer.onLoadConfig()
-paella.data = null;
-
 
 (() => {
 	// Include scripts in header
