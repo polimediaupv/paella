350b4ddeab95b2764ca0a5ed88a2ae6c6cf03734
Fernando Serrano Carpena <ferserc1@gmail.com>
2021-02-12 13:55:22 +0100

    Frame thumbnail container implemented

--- src/js/core/ProgressIndicator.js
+++ src/js/core/ProgressIndicator.js
@@ -1,5 +1,23 @@
-import { DomClass } from 'paella/core/dom';
+import { DomClass, createElementWithHtmlText } from 'paella/core/dom';
 import Events, { bindEvent } from 'paella/core/Events';
+import { secondsToTime } from 'paella/core/utils';
+
+function updateFrameThumbnail(offsetX,time) {
+	this._frameThumbnail.style.display = "block";
+	const thumbWidth = this._frameThumbnail.getBoundingClientRect().width;
+	const playbackBar = this.playbackBar;
+	const { top, left, bottom, width, height } = playbackBar.getBoundingClientRect();
+	const centerX = width / 2;
+	
+	this.frameThumbnail.style.bottom = `${ height }px`;
+	if (centerX > offsetX) {
+		this.frameThumbnail.style.left = `${ offsetX }px`;		
+	}
+	else {
+		this.frameThumbnail.style.left = `${ offsetX - thumbWidth }px`;
+	}
+
+}
 
 export default class ProgressIndicator extends DomClass {
 	constructor(player, parent) {
@@ -12,15 +30,21 @@ export default class ProgressIndicator extends DomClass {
 			<div class="progress-indicator-content"></div>
 		</div>
 		<canvas class="progress-canvas canvas-layer-1"></canvas>
+		<div class="progress-indicator-timer">00:00</div>
 		`;
 		super(player, { attributes, children, parent });
 		
+		this._frameThumbnail = createElementWithHtmlText('<div class="frame-thumbnail"></div>', player.containerElement);
+		this._frameThumbnail.style.display = "none";
+		this._frameThumbnail.style.position = "absolute";
+			
 		this._canvas = [
 			this.element.getElementsByClassName("progress-canvas")[0],
 			this.element.getElementsByClassName("progress-canvas")[1]
 		];
 		this._progressContainer = this.element.getElementsByClassName("progress-indicator-container")[0];
 		this._progressIndicator = this.element.getElementsByClassName("progress-indicator-content")[0];
+		this._progressTimer = this.element.getElementsByClassName("progress-indicator-timer")[0];
 		
 		this.onResize();
 	
@@ -40,12 +64,16 @@ export default class ProgressIndicator extends DomClass {
 		bindEvent(this.player, Events.TIMEUPDATE, async ({ currentTime }) => {
 			if (!drag) {
 				await updateProgressIndicator(currentTime);
+				const formattedTime = secondsToTime(currentTime);
+				this.progressTimer.innerHTML = formattedTime;
 			}
 		});
 		
 		bindEvent(this.player, Events.SEEK, async ({ prevTime, newTime }) => {
 			if (!drag) {
 				await updateProgressIndicator(newTime);
+				const formattedTime = secondsToTime(currentTime);
+				this.progressTimer.innerHTML = formattedTime;
 			}
 		});
 		
@@ -55,11 +83,13 @@ export default class ProgressIndicator extends DomClass {
 			await updateProgressIndicator(newTime);
 		});
 		
+		this.progressContainer._progressIndicator = this;
 		this.progressContainer.addEventListener("mousemove", async (evt) => {
+			const newTime = await positionToTime(evt.offsetX);
 			if (drag) {
-				const newTime = await positionToTime(evt.offsetX);
 				await updateProgressIndicator(newTime);
 			}
+			updateFrameThumbnail.apply(this, [evt.offsetX,newTime]);
 		});
 		
 		this.progressContainer.addEventListener("mouseup", async (evt) => {
@@ -75,7 +105,12 @@ export default class ProgressIndicator extends DomClass {
 				await player.videoContainer.setCurrentTime(newTime);
 				drag = false;
 			}
+			this.frameThumbnail.style.display = "none";
 		});
+	}
+	
+	get playbackBar() {
+		return this.element.parentElement;
 	}
 	
 	get canvasLayer0() {
@@ -90,8 +125,16 @@ export default class ProgressIndicator extends DomClass {
 		return this._progressIndicator;
 	}
 	
+	get progressTimer() {
+		return this._progressTimer;
+	}
+	
 	get progressContainer() {
 		return this._progressContainer;
+	}
+	
+	get frameThumbnail() {
+		return this._frameThumbnail;
 	}
 	
 	onResize() {
--- src/js/core/utils.js
+++ src/js/core/utils.js
@@ -75,3 +75,13 @@ export function setupAutoHideUiTimer(player, hideUiTimePropertyName = "hideUiTim
         await player.showUserInterface();
     });
 }
+
+export function secondsToTime(timestamp) {
+    const hours = Math.floor(timestamp / 60 / 60);
+    const minutes = Math.floor(timestamp / 60) - hours * 60;
+    const seconds = Math.floor(timestamp % 60);
+    return  (hours>0 ? hours.toString().padStart(2,'0') + ":" : "") +
+            minutes.toString().padStart(2,'0') + ":" +
+            seconds.toString().padStart(2,'0');
+}
+
